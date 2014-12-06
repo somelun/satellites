cd0b2fdda0fcbf08646eb34025976083d2ff30a1
griff <rpuxaxa@gmail.com>
2014-12-06 16:31:09 +0000

    some logic

--- Assets/Scripts/SatelliteLogic.cs
+++ Assets/Scripts/SatelliteLogic.cs
@@ -1,13 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+
 public class SatelliteLogic : MonoBehaviour {
+	public float _speed = 20.0f;
+	public float _orbitHeight = 4.0f;
+	
+	private GameObject _planet;
 
 	void Start () {
-	
+		_planet = GameObject.FindGameObjectsWithTag("Planet")[0];
 	}
 	
 	void Update() {
-        transform.RotateAround(Vector3.zero, Vector3.up, 20 * Time.deltaTime);
+        transform.RotateAround(_planet.transform.position, Vector3.up, _speed * Time.deltaTime);
     }
 }
