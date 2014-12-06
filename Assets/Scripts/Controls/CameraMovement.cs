f4ab8409fca96b5bbecda52c8d5c66be5bec9116
some_lun <info@lunatic.su>
2014-12-06 15:46:31 +0000

    Add test scene, add few scripts

---
+++ Assets/Scripts/Controls/CameraMovement.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMovement : MonoBehaviour {
+
+	// public 
+
+	private void Start() {
+		//
+	}
+	
+	private void Update() {
+		transform.RotateAround(Vector3.zero, Vector3.up, 20 * Time.deltaTime);
+	}
+
+}
---
+++ Assets/Scripts/Controls/UserControls.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UserControls : MonoBehaviour {
+
+	private void Start() {
+		//
+	}
+	
+	private void Update() {
+		//
+	}
+}
