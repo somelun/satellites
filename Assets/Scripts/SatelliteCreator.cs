34e224b946ca24b2f8defcd24e36920219375ecd
some_lun <info@lunatic.su>
2014-12-06 17:50:29 +0000

    Add sattelites creator

---
+++ Assets/Scripts/SatelliteCreator.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SatelliteCreator : MonoBehaviour {
+
+	public GameObject satellite;
+
+	private void Start () {
+		Vector3 startPosition = new Vector3(0, 0, -19);
+		CreateSatellite(startPosition);
+	}
+
+	private void CreateSatellite(Vector3 position) {
+		GameObject.Instantiate(satellite, position, Quaternion.identity);
+	}
+
+}
