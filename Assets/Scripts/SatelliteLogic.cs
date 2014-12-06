d4be4dc03e7186f01a0aede42628c00ad776c42f
griff <rpuxaxa@gmail.com>
2014-12-06 15:49:57 +0000

    satellite logic

---
+++ Assets/Scripts/SatelliteLogic.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SatelliteLogic : MonoBehaviour {
+
+	void Start () {
+	
+	}
+	
+	void Update() {
+        transform.RotateAround(Vector3.zero, Vector3.up, 20 * Time.deltaTime);
+    }
+}
